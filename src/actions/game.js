b5bb90f33c62bcb723e56471168968eeeed85843
Max Claus Nunes <maxcnunes@gmail.com>
2015-11-08 10:28:32 -0200

    Move logic of start game and answer to backend

--- src/actions/game.js
+++ src/actions/game.js
@@ -1,60 +1,41 @@
-export const START_GAME = 'START_GAME';
-export const ANSWER_CARD = 'ANSWER_CARD';
+import axios from 'axios';
+export const START_GAME_REQUEST = 'START_GAME_REQUEST';
+export const START_GAME_SUCCESS = 'START_GAME_SUCCESS';
+export const START_GAME_FAILURE = 'START_GAME_FAILURE';
+export const ANSWER_CARD_REQUEST = 'ANSWER_CARD_REQUEST';
+export const ANSWER_CARD_SUCCESS = 'ANSWER_CARD_SUCCESS';
+export const ANSWER_CARD_FAILURE = 'ANSWER_CARD_FAILURE';
 
-export function startGame() {
-  return (dispatch) => {
-    dispatch({
-      type: START_GAME,
-      game: {
-        cards: [
-          {
-            soundtrack: 'https://soundcloud.com/tfrady/harry-potter-soundtrack-hedwigs-theme',
-            options: [
-              { movie: 'tt3659388', isCorrect: false }, // The Martian
-              { movie: 'tt0241527', isCorrect: true }, // Harry Potter
-              { movie: 'tt0120737', isCorrect: false }, // Lord of the Rings
-              { movie: 'tt0363771', isCorrect: false } // Narnia
-            ]
-          },
-          {
-            soundtrack: 'https://soundcloud.com/alexpfeffer/the-godfather-love-theme-feat',
-            options: [
-              { movie: 'tt0099685', isCorrect: false },
-              { movie: 'tt0111161', isCorrect: false },
-              { movie: 'tt0068646', isCorrect: true }, // The godfather
-              { movie: 'tt0110912', isCorrect: false }
-            ]
-          },
-          {
-            soundtrack: 'https://soundcloud.com/championdnb/true-survivor-champion',
-            options: [
-              { movie: 'tt0090728', isCorrect: false },
-              { movie: 'tt3416742', isCorrect: false },
-              { movie: 'tt3472226', isCorrect: true },
-              { movie: 'tt1807165', isCorrect: false }
-            ]
-          },
-          {
-            soundtrack: 'https://soundcloud.com/hollywoodrecpromo/idina-menzel-let-it-go-from',
-            options: [
-              { movie: 'tt1772341', isCorrect: false },
-              { movie: 'tt1217209', isCorrect: false },
-              { movie: 'tt2294629', isCorrect: true },
-              { movie: 'tt0398286', isCorrect: false }
-            ]
-          },
-        ]
-      }
-    });
+export function startGame(username) {
+  return async dispatch => {
+    try {
+      dispatch({ type: START_GAME_REQUEST, username });
+      const result = await axios({
+        url: `/api/game`,
+        method: 'POST',
+        data: { username: 'maxcnunes' }
+      });
+      dispatch({ type: START_GAME_SUCCESS, game: result.data });
+    } catch (error) {
+      dispatch({ type: START_GAME_FAILURE, error });
+    }
   };
 }
 
 
 export function answerCard(answer) {
-  return (dispatch) => {
-    dispatch({
-      type: ANSWER_CARD,
-      answer
-    });
+  return async (dispatch, getState) => {
+    try {
+      const gameId = getState().game._id;
+      dispatch({ type: ANSWER_CARD_REQUEST, answer });
+      const result = await axios({
+        url: `/api/game/${gameId}/answer`,
+        method: 'POST',
+        data: { answer }
+      });
+      dispatch({ type: ANSWER_CARD_SUCCESS, ...result.data });
+    } catch (error) {
+      dispatch({ type: ANSWER_CARD_FAILURE, error });
+    }
   };
 }
--- src/reducers/game.js
+++ src/reducers/game.js
@@ -1,21 +1,21 @@
-import { START_GAME, ANSWER_CARD } from '../actions/game';
+import { START_GAME_SUCCESS, ANSWER_CARD_SUCCESS } from '../actions/game';
 import { FINISH_TURN } from '../actions/turn';
 
 const INITIAL_STATE = {};
 
 export default function counter(state = INITIAL_STATE, action) {
   switch (action.type) {
-  case START_GAME:
+  case START_GAME_SUCCESS:
     return {
       ...state,
       ...action.game,
       currentMatch: 0,
       isOver: false
     };
-  case ANSWER_CARD:
+  case ANSWER_CARD_SUCCESS:
     return state;
   case FINISH_TURN:
-    if (state.currentMatch === state.cards.length - 1) {
+    if (state.currentMatch === state.questions.length - 1) {
       return { isOver: true };
     }
 
