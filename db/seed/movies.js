110fdd39a3cc5b61cf4a201286cc091019df5c15
Rafael Seemann <rseemann@gmail.com>
2015-11-08 19:25:28 -0200

    Seed more movies

--- db/seed/movies.js
+++ db/seed/movies.js
@@ -2,10 +2,6 @@ import {Movie} from '../../server/models';
 
 
 const MOVIES = [
-  { imdbId: 'tt0241527', soundtrack: 'https://soundcloud.com/tfrady/harry-potter-soundtrack-hedwigs-theme' },
-  { imdbId: 'tt0068646', soundtrack: 'https://soundcloud.com/alexpfeffer/the-godfather-love-theme-feat' },
-  { imdbId: 'tt3472226', soundtrack: 'https://soundcloud.com/championdnb/true-survivor-champion' },
-  { imdbId: 'tt2294629', soundtrack: 'https://soundcloud.com/hollywoodrecpromo/idina-menzel-let-it-go-from' },
   { imdbId: 'tt0172495', soundtrack: 'https://soundcloud.com/tarek-alaa-1/gladiator-soundtrack-elysium' },
   { imdbId: 'tt0468569', soundtrack: 'https://soundcloud.com/antovolk/the-dark-knight-batman-theme' },
   { imdbId: 'tt1375666', soundtrack: 'https://soundcloud.com/mohamed-shibo/inception-soundtrack-the-dream' },
@@ -20,9 +16,19 @@ const MOVIES = [
   { imdbId: 'tt0064276', soundtrack: 'https://soundcloud.com/chipmunkcheeks228/steppenwolf-born-to-be-wild' },
   { imdbId: 'tt0075148', soundtrack: 'https://soundcloud.com/usn_audioproduction/rocky-theme-song' },
   { imdbId: 'tt0110912', soundtrack: 'https://soundcloud.com/kupukupuku/pulp-fiction-opening-theme' },
-  { imdbId: 'tt0097576', soundtrack: 'https://soundcloud.com/ethanmessier/indiana-jones-and-the-raiders' },
+  { imdbId: 'tt0082971', soundtrack: 'https://soundcloud.com/ethanmessier/indiana-jones-and-the-raiders' },
   { imdbId: 'tt0080684', soundtrack: 'https://soundcloud.com/christophe-catherine/star-wars-imperial-march-full' },
-  { imdbId: 'tt0061852', soundtrack: 'https://soundcloud.com/nightlife-official/the-bare-necessities' }
+  { imdbId: 'tt1104001', soundtrack: 'https://soundcloud.com/robotaki/tron-legacy-daft-punk-derezzed' },
+  { imdbId: 'tt0147800', soundtrack: 'https://soundcloud.com/reham-eissa-1/10-things-i-hate-about-you-cant-take-my-eyes-off-you-heath-ledger' },
+  { imdbId: 'tt2322441', soundtrack: 'https://soundcloud.com/kiana/love-me-like-you-do-from-fifty-shades-of-grey-ellie-goulding-cover' },
+  { imdbId: 'tt2582846', soundtrack: 'https://soundcloud.com/garima_22/charli-xcx-boom-clap-ost-the-fault-in-our-stars' },
+  { imdbId: 'tt0055928', soundtrack: 'https://soundcloud.com/tomschlueter/james-bond-theme' },
+  { imdbId: 'tt0107290', soundtrack: 'https://soundcloud.com/revanth-prasad/jurassic-park-theme-song' },
+  { imdbId: 'tt0087332', soundtrack: 'https://soundcloud.com/fororchestra/ghostbusters-theme-song-for' },
+  { imdbId: 'tt0120338', soundtrack: 'https://soundcloud.com/suja-1/titanic-song-with-lyrics' },
+  { imdbId: 'tt0463985', soundtrack: 'https://soundcloud.com/mohamad-da-1/fast-and-furious-3-tokyo-drift' },
+  { imdbId: 'tt0211915', soundtrack: 'https://soundcloud.com/giorgio-iannelli/yann_tiersen-amelie_piano_version' },
+  { imdbId: 'tt1951265', soundtrack: 'https://soundcloud.com/starwarspunk/the-hanging-tree-from-the-hunger-games-mockingjay-piano-cover-by-dani' }
 ];
 
 /*
